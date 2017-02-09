106433fb0dcfc714d0d1f0e46545fbb9cce6d3df
CorinneKelly <corinnemariekelly@gmail.com>
2017-02-09 18:10:33 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+class Student < User
+	attr_accessor :name, :knowledge, :teacher
+	def initialize
+		super
+		# runs initalize of User with the additions added in this method
+		@knowledge = []
+		# @knowledge << self.teacher.teach
+	end
+
+	def learn(something)
+		@knowledge << something
+	end
 
 end
---
+++ lib/user.rb
@@ -0,0 +1,7 @@
+class User
+	attr_accessor :first_name, :last_name
+	def initialize
+		# @first_name = name.split[0]
+		# @last_name = name.split[-1]
+	end
+end
